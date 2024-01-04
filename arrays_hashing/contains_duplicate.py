0d12d5847d9602c744c6020111edcfb94c5bdf75
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-01-04 05:51:23 +0530

    Create contains_duplicate.py

---
+++ arrays_hashing/contains_duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hash = {}
+        for num in nums:
+            if num in hash:
+                return True
+            else:
+                hash[num] = 0
+        return False
