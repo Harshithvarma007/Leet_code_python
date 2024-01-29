a27beadba6417805789e0d35656c3ca4eb3e0cb4
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-01-29 12:57:50 +0530

    Update same_tree.py

--- Trees/same_tree.py
+++ Trees/same_tree.py
@@ -1,3 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -5,20 +11,9 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        def check(p, q):
-            # If both nodes are None, they are equal
-            if not p and not q:
-                return True
-            
-            # If one of the nodes is None and the other is not, they are not equal
-            if not p or not q:
-                return False
-            
-            # If values are not equal, the trees are not the same
-            if p.val != q.val:
-                return False
-            
-            # Recursively check left and right subtrees
-            return check(p.left, q.left) and check(p.right, q.right)
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return (p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right) )
         
-        return check(p, q)
